2d2afeee6683ae57f43e7d0096f2c0b11c99105f
Sacha Ponniah <sponniah812@gmail.com>
2022-01-08 19:07:50 +0000

    All tests complete and passed.

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,4 +7,58 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should have working arithmetical operator buttons', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '5')
+  })
+
+  it('multiple operations can be chained together', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    cy.get('#decimal').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '12.5');
+  })
+
+  it('output should be as expected for negative numbers', () => {
+    cy.get('#number5').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number8').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-3');
+  })
+
+  it('output should be as expected for very large numbers', () => {
+    cy.get('#number1').click();
+    cy.get('#number2').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number1').click();
+    cy.get('#number2').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '14400000000');
+  })
+
+  it('it should display an error message in exceptional circumstances', () => {
+    cy.get('#number5').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', 'error');
+  })
 })
